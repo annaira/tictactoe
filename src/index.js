26b461ca76665cdbad200dca5031f304afbfafa0
Anna Ira <annairahurnaus@gmail.com>
2018-11-05 23:29:57 +0100

    Showing the Past Moves

--- src/index.js
+++ src/index.js
@@ -76,7 +76,7 @@ class Game extends React.Component {
             xIsNext: true,
         };
     }
-    
+
     handleClick(i) {
         const history = this.state.history;
         const current = history[history.length-1];
@@ -98,6 +98,17 @@ class Game extends React.Component {
         const current = history[history.length - 1];
         const winner = calculateWinner(current.squares);
 
+        const moves = history.map((step, move) => {
+            const desc = move ?
+                'Go to move #' + move:
+                'Go to game start';
+            return (
+                <li>
+                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                </li>
+            );
+        });
+
         let status;
         if (winner) {
             status = 'Winner: ' + winner;
@@ -115,7 +126,7 @@ class Game extends React.Component {
                 </div>
                 <div className="game-info">
                     <div>{status}</div>
-                    <ol>{/* TODO */}</ol>
+                    <ol>{moves}</ol>
                 </div>
             </div>
         );
